881646987f0a300c1279cb4fb83eb6ba59337717
Ahmed-517 <ahmed195041@feng.bu.edu.eg>
2025-04-09 09:34:54 +0200

    `Refactor getUserSubscription controller to accept req, res, next and fix subscription query`

--- controllers/subscription.controller.js
+++ controllers/subscription.controller.js
@@ -13,16 +13,20 @@ export const createSubscription = async (req, res, next) => {
   }
 };
 
-export const getUserSubscription = async ()=> {
+export const getUserSubscription = async (req, res, next)=> {
 
   try {
     if (req.user.id !== req.params.id) {
       const error = new Error("You are not the owner of this account")
+      error.status = 401;
+      throw error;
     }
 
-    const subscription = await Subscription.find({ user: req.user._id });
+    const subscriptions = await Subscription.find({ user: req.params.id });
+
+    res.status(200).json({success:true, data: subscriptions})
 
   } catch(e) {
-
+    next(e)
   }
 }
