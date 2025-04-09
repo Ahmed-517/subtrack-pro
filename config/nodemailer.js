5a4f3a094bf5ebce7ba57b6179501353b9dbdbc2
Ahmed-517 <ahmed195041@feng.bu.edu.eg>
2025-04-09 14:00:27 +0200

    `feat: added email password to env and updated nodemailer to use it`

--- config/nodemailer.js
+++ config/nodemailer.js
@@ -1,9 +1,14 @@
 import nodemailer from "nodemailer";
+import { EMAIL_PASSWORD } from "./env.js";
+
+export const accountEmail = "AhmedGamalGoda1@gmail.com";
 
 const transporter = nodemailer.createTransport({
   service: "gmail",
   auth: {
     user: "AhmedGamalGoda1@gmail.com",
-    pass: "AhmedGamalGoda1",
+    pass: EMAIL_PASSWORD,
   },
 });
+
+export default transporter;
--- controllers/workflow.controller.js
+++ controllers/workflow.controller.js
@@ -5,6 +5,7 @@ const { serve } = require("@upstash/workflow/express");
 
 import dayjs from "dayjs";
 import Subscription from "../models/subscription.model.js";
+import { sendReminderEmail } from "../utils/send-email.js";
 
 const REMINDERS = [7, 5, 2, 1];
 
@@ -35,7 +36,14 @@ export const sendReminders = serve(async (context) => {
         reminderDate
       );
     }
-    await triggerReminder(context, `Reminder ${daysBefore} days before`);
+
+    if (dayjs().isSame(reminderDate, "day")) {
+      await triggerReminder(
+        context,
+        `${daysBefore} days before reminder`,
+        subscription
+      );
+    }
   }
 });
 
@@ -50,8 +58,14 @@ const sleepUntilReminder = async (context, label, date) => {
   await context.sleepUntil(label, date.toDate());
 };
 
-const triggerReminder = async (context, label) => {
-  return await context.run(label, () => {
+const triggerReminder = async (context, label, subscription) => {
+  return await context.run(label, async () => {
     console.log(`Triggering reminder ${label}`);
+
+    await sendReminderEmail({
+      to: subscription.user.Email,
+      type: label,
+      subscription,
+    });
   });
 };
